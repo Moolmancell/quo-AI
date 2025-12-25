0b9e8c817bf1ef4dadaa837d6e098195dd1687ff
Richmond <richmondclaudebodot@gmail.com>
2025-12-25 15:38:31 +0800

    Add min-h-screen class to signup page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,7 +1,7 @@
 import SignUpForm from "@/components/signup/SignUpForm"
 
 export default function SignUp() {
-    return <main className="font-sans flex flex-col items-center justify-center p-4">
+    return <main className="font-sans min-h-screen flex flex-col items-center justify-center p-4">
         <div className="max-w-md w-full">
             <SignUpForm />
         </div>
