4d4b70350099161b84555c786efe307826a01c07
Richmond <richmondclaudebodot@gmail.com>
2025-12-26 23:24:40 +0800

    Add NotFound component for error handling

---
+++ components/not found/NotFound.tsx
@@ -0,0 +1,33 @@
+import { RotateCw } from "lucide-react"
+import { CloudOff } from "lucide-react"
+import { Button } from "@/components/ui/Button"
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from "@/components/ui/Empty"
+
+export function NotFound({className, onClick}: {className?: string, onClick?: () => void }) {
+  return (
+    <Empty className={className}>
+      <EmptyHeader>
+        <EmptyMedia variant="icon">
+          <CloudOff className="w-12 h-12 text-muted-foreground"/>
+        </EmptyMedia>
+        <EmptyTitle>Something Went Wrong</EmptyTitle>
+        <EmptyDescription>
+          Try refreshing the page or come back later.
+        </EmptyDescription>
+      </EmptyHeader>
+      <EmptyContent>
+        <Button size="sm" variant="outline" onClick={onClick}>
+          <RotateCw />
+          Refresh Page
+        </Button>
+      </EmptyContent>
+    </Empty>
+  )
+}
