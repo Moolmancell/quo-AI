83983a4762db4f69b30b691b15f30567ca9c36c8
Richmond <richmondclaudebodot@gmail.com>
2025-12-26 23:24:48 +0800

    Add ProtectedRoute component for session management and redirection

---
+++ components/protected/ProtectedRoute.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { Spinner } from "@/components/ui/Spinner";
+import { NotFound } from "@/components/not found/NotFound";
+
+export function ProtectedRoute({ children }: { children: React.ReactNode }) {
+    const {
+        data: session,
+        isPending, //loading state
+        error, //error object
+        refetch //refetch the session
+    } = authClient.useSession()   
+    
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!isPending && !session) {
+            router.push("/login");
+        }
+    }, [session, isPending, router]);
+
+    if (isPending) {
+        return <Spinner className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 size-8" />; // Replace with a Spinner
+    }
+
+    if (error) {
+        return <NotFound onClick={() => refetch()} className="fixed top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2"/>
+    }
+
+    return session ? <>{children}</> : null;
+}
