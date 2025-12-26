2164050413b88e6ac2f90308798431230b606c85
Richmond <richmondclaudebodot@gmail.com>
2025-12-26 22:21:24 +0800

    Add a link that lets the user go back to the landing page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,14 @@
 import LogInForm from "@/components/login/LogInForm"
+import { ArrowLeft } from "lucide-react"
+import Link from "next/link"
 
 export default function LogIn() {
-    return <main className="font-sans min-h-screen flex flex-col items-center justify-center p-4">
+    return <main className="relativefont-sans min-h-screen flex flex-col items-center justify-center p-4">
         <div className="max-w-md w-full">
+            <Link href='/' className="flex flex-row gap-2 text-sm hover:underline items-center mb-4">
+                <ArrowLeft className="w-4.5 h-auto" />
+                Back
+            </Link>
             <LogInForm />
         </div>
     </main>
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,8 +1,14 @@
 import SignUpForm from "@/components/signup/SignUpForm"
+import { ArrowLeft } from "lucide-react"
+import Link from "next/link"
 
 export default function SignUp() {
     return <main className="font-sans min-h-screen flex flex-col items-center justify-center p-4">
         <div className="max-w-md w-full">
+            <Link href='/' className="flex flex-row gap-2 text-sm hover:underline items-center mb-4">
+                <ArrowLeft className="w-4.5 h-auto" />
+                Back
+            </Link>
             <SignUpForm />
         </div>
     </main>
