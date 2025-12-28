b6892cc2156006c6ff2f8bfc38f351e24beba802
Richmond <richmondclaudebodot@gmail.com>
2025-12-28 16:42:12 +0800

    Wrap LogIn and SignUp pages in ProtectedRoute for only guest visits

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,15 +1,18 @@
 import SignUpForm from "@/components/signup/SignUpForm"
 import { ArrowLeft } from "lucide-react"
 import Link from "next/link"
+import { ProtectedRoute } from "@/components/protected/ProtectedRoute"
 
 export default function SignUp() {
-    return <main className="font-sans min-h-screen flex flex-col items-center justify-center p-4">
-        <div className="max-w-md w-full">
-            <Link href='/' className="flex flex-row gap-2 text-sm hover:underline items-center mb-4">
-                <ArrowLeft className="w-4.5 h-auto" />
-                Back
-            </Link>
-            <SignUpForm />
-        </div>
-    </main>
+    return <ProtectedRoute reverse>
+        <main className="font-sans min-h-screen flex flex-col items-center justify-center p-4">
+            <div className="max-w-md w-full">
+                <Link href='/' className="flex flex-row gap-2 text-sm hover:underline items-center mb-4">
+                    <ArrowLeft className="w-4.5 h-auto" />
+                    Back
+                </Link>
+                <SignUpForm />
+            </div>
+        </main>
+    </ProtectedRoute>
 }
