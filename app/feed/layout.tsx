787d8801fa3ede26556f3a9cde7d3fcff5b92cdb
Richmond <richmondclaudebodot@gmail.com>
2025-12-29 19:03:26 +0800

    Add Chat, Feed, and Search pages with basic content structure

--- app/feed/layout.tsx
+++ app/feed/layout.tsx
@@ -1,11 +1,23 @@
 import { ProtectedRoute } from "@/components/protected/ProtectedRoute";
+import MobileNavBar from "@/components/feed/MobileNavBar";
 
-export default function FeedLayout() {
-    return <ProtectedRoute>
-            <div>
-                <nav>
+export default function FeedLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <ProtectedRoute>
+            <div className="flex flex-col min-h-screen bg-background">
+                
+                <main className="flex-1 pb-17 sm:pb-0 sm:pt-16">
+                    {children}
+                </main>
 
-                </nav>
+                <div className="fixed bottom-0 left-0 right-0 z-50 bg-background sm:hidden">
+                    <MobileNavBar />
+                </div>
+
+                <div className="hidden sm:block fixed top-0 left-0 right-0 z-50 border-b bg-background">
+                     {/* <DesktopNavBar /> */}
+                </div>
             </div>
-    </ProtectedRoute>;
+        </ProtectedRoute>
+    );
 }
