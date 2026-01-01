33d7c73275f1a1e1074b3eb5aa1effd5266a5a0a
Richmond <richmondclaudebodot@gmail.com>
2026-01-01 19:58:58 +0800

    Create mock handlers for favicon and featured image

--- mocks/handlers/feed.ts
+++ mocks/handlers/feed.ts
@@ -1,5 +1,6 @@
 import { http, HttpResponse } from 'msw';
 import feedData from '../data/feed.json';
+import { getDiceBearAvatar } from '@/lib/dicebear';
 
 const BASE_URL = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api`;
 
@@ -7,4 +8,19 @@ export const feedHandlers = [
   http.get(`${BASE_URL}/get-feed`, () => {
     return HttpResponse.json(feedData, { status: 200 });
   }),
+
+  http.get(`${BASE_URL}/get-featured-image`, (req) => {
+    return HttpResponse.json({
+      featuredImageUrl: 'https://picsum.photos/400'
+    }, { status: 200 });
+  }),
+
+  http.get(`${BASE_URL}/get-favicon-image`, (req) => {
+
+    const avatar = getDiceBearAvatar('123')
+
+    return HttpResponse.json({
+      faviconImageUrl: avatar
+    }, { status: 200 });
+  })
 ];
