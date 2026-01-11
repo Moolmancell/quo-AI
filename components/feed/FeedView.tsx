db737d0ed4f72be417d55524a1efa4a7049d49c1
Richmond <richmondclaudebodot@gmail.com>
2026-01-11 18:19:36 +0800

    Create bookmark animation when double click

--- components/feed/FeedView.tsx
+++ components/feed/FeedView.tsx
@@ -1,9 +1,11 @@
-import { motion, PanInfo } from "motion/react";
+import { motion, PanInfo, AnimatePresence } from "motion/react";
 import { Spinner } from "../ui/Spinner";
 import { FeedCard } from "./FeedCard";
 import { FeedContentProps } from "@/interfaces/feed/FeedContentProps";
-import { useState } from "react";
+import { Bookmark } from "lucide-react";
 import { useBookmarks } from "@/hooks/feed/useBookmarks";
+import { createPortal } from "react-dom";
+import { useState } from "react";
 
 export function FeedView({ className, handleDragEnd, currentPage, isFetchingMore, feed, y }: {
     className: string,
@@ -15,6 +17,12 @@ export function FeedView({ className, handleDragEnd, currentPage, isFetchingMore
 }) {
 
     const { bookmarkedUrls, toggleBookmark, toggleBookmarkDoubleClick } = useBookmarks();
+    const [bookmarkVisible, setBookmarkVisible] = useState(false);
+
+    const handleBookmarkAnimation = () => {
+        setBookmarkVisible(true);
+        setTimeout(() => setBookmarkVisible(false), 700)
+    }
 
     const height = typeof window !== "undefined"
         ? window.innerHeight
@@ -38,6 +46,29 @@ export function FeedView({ className, handleDragEnd, currentPage, isFetchingMore
             style={{ y }}
             className={className}
         >
+
+            {typeof window !== "undefined" &&
+                createPortal(
+                    <AnimatePresence>
+                        {bookmarkVisible &&
+                            <motion.div
+                                className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50"
+                                key='bookmark'
+                                initial={{ opacity: 0, scale: 0 }}
+                                animate={{ opacity: 1, scale: 3.5 }}
+                                exit={{ opacity: 0, scale: 0 }}
+                            >
+                                <Bookmark
+                                    className="fill-white text-white  drop-shadow-md"
+                                />
+                            </motion.div>
+                        }
+                    </AnimatePresence>
+                    ,
+                    document.body
+                )
+            }
+
             {visibleFeed.map((item, i) => {
                 const index = Math.max(0, currentPage - 1) + i;
                 console.log(visibleFeed)
@@ -46,7 +77,10 @@ export function FeedView({ className, handleDragEnd, currentPage, isFetchingMore
                         key={index}
                         style={{ top: index * height }}
                         className="absolute w-full h-dvh flex items-center justify-center p-4"
-                        onDoubleClick={() => toggleBookmarkDoubleClick(item.src)}
+                        onDoubleClick={() => {
+                            toggleBookmarkDoubleClick(item.src)
+                            handleBookmarkAnimation();
+                        }}
                     >
                         <FeedCard {...item} isBookmarked={bookmarkedUrls.has(item.src)} toggleBookmark={toggleBookmark} />
                     </motion.div>
