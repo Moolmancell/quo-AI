6e55d92016dc5b0eaf22eca8f5855906a7f5f360
Richmond <richmondclaudebodot@gmail.com>
2026-01-19 12:58:07 +0800

    Wrap FeedLayout content with InterestCheck component

--- app/feed/layout.tsx
+++ app/feed/layout.tsx
@@ -1,24 +1,28 @@
 import { ProtectedRoute } from "@/components/protected/ProtectedRoute";
 import MobileNavBar from "@/components/feed/MobileNavBar";
 import DesktopNavBar from "@/components/feed/DesktopNavBar";
+import { InterestCheck } from "@/components/interest_check/InterestCheck";
+import { Inter } from "next/font/google";
 
 export default function FeedLayout({ children }: { children: React.ReactNode }) {
     return (
         <ProtectedRoute>
-            <div className="flex flex-col min-h-screen bg-background">
-                
-                <main className="flex-1 pb-17 sm:pb-0 sm:pt-16">
-                    {children}
-                </main>
+            <InterestCheck>
+                <div className="flex flex-col min-h-screen bg-background">
 
-                <div className="fixed bottom-0 left-0 right-0 z-50 bg-background sm:hidden">
-                    <MobileNavBar />
-                </div>
+                    <main className="flex-1 pb-17 sm:pb-0 sm:pt-16">
+                        {children}
+                    </main>
 
-                <div className="hidden sm:block fixed top-0 left-0 right-0 z-50 bg-background">
-                     <DesktopNavBar /> 
+                    <div className="fixed bottom-0 left-0 right-0 z-50 bg-background sm:hidden">
+                        <MobileNavBar />
+                    </div>
+
+                    <div className="hidden sm:block fixed top-0 left-0 right-0 z-50 bg-background">
+                        <DesktopNavBar />
+                    </div>
                 </div>
-            </div>
+            </InterestCheck>
         </ProtectedRoute>
     );
 }
