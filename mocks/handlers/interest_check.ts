b18e9ed2e265da205d4b45ef0e9df4aa3d6ca476
Richmond <richmondclaudebodot@gmail.com>
2026-01-21 12:45:47 +0800

    Add mock urls for post:/submit-interests and get:/generate-interests

--- mocks/handlers/interest_check.ts
+++ mocks/handlers/interest_check.ts
@@ -1,9 +1,24 @@
 import { http, HttpResponse } from 'msw';
+import { int } from 'zod';
 
-const BASE_URL = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api`;
+const BASE_URL = `${process.env.NEXT_PUBLIC_API_URL}/api`;
+
 
 export const interestCheckHandlers = [
+
   http.get(`${BASE_URL}/interests/:userID`, () => {
-    return HttpResponse.json({interests: ['philosophy', 'software development']}, { status: 200 });
+    //NOTE: for no interests selected, return empty array
+    return HttpResponse.json({ interests: [] }, { status: 200 });
+  }),
+
+  http.post(`${BASE_URL}/submit-interests/:userID`, async ({ request }) => {
+    const body = await request.json() as { interests: string[] }
+    console.log('received request body interests', body);
+    return HttpResponse.json({ status: 200 });
+  }),
+
+  http.get(`${BASE_URL}/generate-interests`, async () => {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    return HttpResponse.json({ interests: ["Music", "Travel", "Sports", "Cooking", "Fitness", "Movies"] }, { status: 200 });
   }),
 ];
