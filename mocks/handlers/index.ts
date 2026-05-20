fe625c02d2e7a067819d1197d3e97f3194497fe2
Richmond <richmondclaudebodot@gmail.com>
2026-05-20 14:11:35 +0800

    Add searchHandlers to handlers in the index of mocks

--- mocks/handlers/index.ts
+++ mocks/handlers/index.ts
@@ -1,10 +1,12 @@
 import { authHandlers } from './auth';
 import { feedHandlers } from './feed';
 import { interestCheckHandlers } from './interest_check';
+import { searchHandlers } from './search';
 
 export const handlers = [
   ...authHandlers,
   ...feedHandlers,
   ...interestCheckHandlers,
+  ...searchHandlers
   // Add more handlers here as you build them
 ];
