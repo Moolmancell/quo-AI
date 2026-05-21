0c149172d33fcd5977763cfb81b95fe1127995f3
Richmond <richmondclaudebodot@gmail.com>
2026-05-21 21:09:49 +0800

    Update image remote patterns in next.config.ts to allow all domains

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-  remotePatterns: [
-    {
-      protocol: 'https',
-      hostname: 'picsum.photos', 
-    },
-  ],
-},
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // Matches any domain
+      },
+      {
+        protocol: 'http',
+        hostname: '**', // Matches any domain
+      }
+    ],
+  },
 };
 
 export default nextConfig;
