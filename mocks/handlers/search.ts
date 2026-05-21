0f41a06f1640eb068b3dd3cf4a94dbba3e995b8a
Richmond <richmondclaudebodot@gmail.com>
2026-05-21 14:55:07 +0800

    Update the search url in mocks

--- mocks/handlers/search.ts
+++ mocks/handlers/search.ts
@@ -1,7 +1,7 @@
 import { http, HttpResponse } from 'msw';
 import { set } from 'zod';
 
-const BASE_URL = `${process.env.NEXT_PUBLIC_API_URL}/api`;
+const BASE_URL = `${process.env.NEXT_PUBLIC_API_URL}/api/search`;
 
 //TODO: Change API endpoints to match those in src/app.ts (quo-ai-backend) when created
 export const searchHandlers = [
@@ -19,5 +19,15 @@ export const searchHandlers = [
             "Elden Ring",
             "Video Games"
         ], { status: 200 });
+    }),
+
+    http.delete(`${BASE_URL}/remove-recent-search`, async () => {
+        await new Promise(resolve => setTimeout(resolve, 1000)); // Simulate network delay
+        return HttpResponse.json({}, { status: 200 });
+    }),
+
+    http.post(`${BASE_URL}/clear-recent-search`, async () => {
+        await new Promise(resolve => setTimeout(resolve, 1000)); // Simulate network delay
+        return HttpResponse.json({}, { status: 200 });
     }),
 ];
