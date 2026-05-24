93a00ba3cc3169f765fc0041434e892225ef36fd
Richmond <richmondclaudebodot@gmail.com>
2026-05-24 17:02:26 +0800

    Change /get-feed routes to /feed/get-feed

--- mocks/handlers/feed.ts
+++ mocks/handlers/feed.ts
@@ -6,16 +6,16 @@ const BASE_URL = `${process.env.NEXT_PUBLIC_API_URL}/api`;
 
 //TODO: Change API endpoints to match those in src/app.ts (quo-ai-backend) when created
 export const feedHandlers = [
-  http.get(`${BASE_URL}/get-feed`, () => {
+  http.get(`${BASE_URL}/feed/get-feed`, () => {
     return HttpResponse.json(feedData, { status: 200 });
   }),
 
-  http.post(`${BASE_URL}/add-bookmark`, ({request}) => {
+  http.post(`${BASE_URL}/feed/add-bookmark`, ({request}) => {
     const item = request.body as any
     return HttpResponse.json({item: item}, {status: 200})
   }),
 
-  http.delete(`${BASE_URL}/delete-bookmark`, ({request}) => {
+  http.delete(`${BASE_URL}/feed/delete-bookmark`, ({request}) => {
     const item = request.body as any
     return HttpResponse.json({item: item}, {status: 200})
   })
