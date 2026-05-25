2823be6de996bf096691d89899fb84a9d9b3e1bc
Richmond <richmondclaudebodot@gmail.com>
2026-05-25 16:37:17 +0800

    Add one more variety of mock quotes

--- mocks/handlers/feed.ts
+++ mocks/handlers/feed.ts
@@ -1,13 +1,19 @@
 import { http, HttpResponse } from 'msw';
 import feedData from '../data/feed.json';
+import feedData2 from '../data/feed2.json';
 import { getDiceBearAvatar } from '@/lib/dicebear';
 
 const BASE_URL = `${process.env.NEXT_PUBLIC_API_URL}/api`;
-
+let callCount = 1;
 //TODO: Change API endpoints to match those in src/app.ts (quo-ai-backend) when created
 export const feedHandlers = [
   http.get(`${BASE_URL}/feed/get-feed`, () => {
-    return HttpResponse.json(feedData, { status: 200 });
+    callCount++;
+    if (callCount % 2 === 0) {
+      return HttpResponse.json(feedData2, { status: 200 });
+    } else {
+      return HttpResponse.json(feedData, { status: 200 });
+    }
   }),
 
   http.post(`${BASE_URL}/feed/add-bookmark`, ({request}) => {
