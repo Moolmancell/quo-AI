2e54c3e1b4108dbfab60429c0c4178596dd0c8a6
Richmond <richmondclaudebodot@gmail.com>
2026-05-26 22:11:29 +0800

    Remove Thumbnail and Favicon in ShareCard

--- components/feed/ShareCard.tsx
+++ components/feed/ShareCard.tsx
@@ -19,16 +19,12 @@ interface ShareCardProps extends FeedContentProps {
     ref?: any;
 }
 
-export function ShareCard({ ref, className, datePublished, relativeTime, author, src, publication, quote, favicon, thumbnail } : ShareCardProps) {
+export function ShareCard({ ref, className, datePublished, relativeTime, author, src, publication, quote, favicon, thumbnail }: ShareCardProps) {
 
     return (
         <Card ref={ref} className={`${className} font-sans rounded-none p-0 w-96 gap-0 shadow-none hover:shadow-2xl`}>
             {/* Header: Avatar and Metadata */}
             <CardHeader className="flex items-center gap-2.5 p-3">
-                <Avatar className='size-8'>
-                    <AvatarImage src={favicon || ""} alt={publication} />
-                    <AvatarFallback><Globe className="size-4 text-muted" /></AvatarFallback>
-                </Avatar>
                 <div>
                     <h2 className="text-xs sm:text-sm font-medium text-foreground mb-1 leading-none">{author}</h2>
                     <p className="text-xs text-muted-foreground">{relativeTime}</p>
@@ -39,21 +35,13 @@ export function ShareCard({ ref, className, datePublished, relativeTime, author,
 
             <CardContent className="p-0">
                 {/* Featured Image Section */}
-                <div className="overflow-hidden border bg-muted">
-                    <AspectRatio ratio={4 / 3}>
-                            <Image
-                                src={thumbnail || ""}
-                                alt="Featured Image"
-                                fill
-                                className="transition-opacity duration-300"
-                                style={{ objectFit: 'cover' }}
-                            />
-                    </AspectRatio>
-                </div>
 
                 {/* Content Section */}
                 <div className="p-3">
-                    <p className="text-xs sm:text-sm font-normal text-card-foreground mb-4">"{quote}"</p>
+                    <p
+                        className="text-xs sm:text-sm font-normal text-card-foreground mb-4 line-clamp-5"
+                        dangerouslySetInnerHTML={{ __html: quote }}
+                    />                    
                     <p className="text-xs font-normal text-muted-foreground">{publication}</p>
                 </div>
             </CardContent>
