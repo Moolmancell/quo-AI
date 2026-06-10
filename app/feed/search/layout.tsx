c876dba8a00b7d103ec37f9d1c509526b5e2ac94
Richmond <richmondclaudebodot@gmail.com>
2026-06-10 13:01:38 +0800

    Add Select quote or article/essay feature in search

--- app/feed/search/layout.tsx
+++ app/feed/search/layout.tsx
@@ -6,6 +6,15 @@ import { useRouter, useSearchParams } from "next/navigation"
 import { Button } from "@/components/ui/Button";
 import { SearchIcon } from "lucide-react";
 import { XIcon } from "lucide-react";
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/Select"
+
 
 export default function SearchLayout({ children }: { children: React.ReactNode }) {
     const router = useRouter();
@@ -16,9 +25,10 @@ export default function SearchLayout({ children }: { children: React.ReactNode }
         e.preventDefault();
         const formData = new FormData(e.currentTarget);
         const query = formData.get("q");
-        
+        const type = formData.get("type") || "Articles/Essays";
+
         if (typeof query === "string" && query.trim()) {
-            router.push(`/feed/search?q=${encodeURIComponent(query)}`);
+            router.push(`/feed/search?q=${encodeURIComponent(query)}&type=${encodeURIComponent(type as string)}`);
         }
     };
 
@@ -53,6 +63,17 @@ export default function SearchLayout({ children }: { children: React.ReactNode }
                             )
                         }
                     </div>
+                    <Select defaultValue="Articles/Essays" name="type">
+                        <SelectTrigger className="absolute right-12 top-1/2 -translate-y-1/2 border-none focus:ring-0">
+                            <SelectValue placeholder="Search" />
+                        </SelectTrigger>
+                        <SelectContent className="rounded-sm p-1">
+                            <SelectGroup>
+                                <SelectItem value="Quotes">Quotes</SelectItem>
+                                <SelectItem value="Articles/Essays">Articles/Essays</SelectItem>
+                            </SelectGroup>
+                        </SelectContent>
+                    </Select>
                 </div>
             </form>
 
